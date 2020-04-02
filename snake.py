c4f48bdbdf6f792f7a5bb633e06421d30d4cd365
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-04-02 21:06:38 +0300

    commit

--- snake.py
+++ snake.py
@@ -1,3 +1,52 @@
 import math
 import tkinter as tk
+from tkinter import messagebox
+import random
 import pygame
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self,start, dirnx=1, dirny=0, color=(255,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+    def draw(self, surface, eyes=False):
+        pass
+
+class Snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+    
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+    
+    def draw(self, surface):
+        pass
+
+def drawGrid(w, rows, surface):
+    pass    
+
+def redrawWindow(surface):
+    pass
+
+def randomSnack(rows, item):
+    pass
+
+def messagebox(subject, content):
+    pass
+
+def main():
+    width = 500
+    height = 50
+    win = pygame.display.set_mode((width.height))
+    pass
+
+main()
