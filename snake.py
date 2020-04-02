0c4cf9540b7705763b85ae0431dcb6d9551c6b98
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-04-02 21:53:52 +0300

    second commit

--- snake.py
+++ snake.py
@@ -6,7 +6,7 @@ import pygame
 
 class cube(object):
     rows = 0
-    w = 0
+    width = 0
     def __init__(self,start, dirnx=1, dirny=0, color=(255,0,0)):
         pass
 
@@ -15,7 +15,7 @@ class cube(object):
     def draw(self, surface, eyes=False):
         pass
 
-class Snake(object):
+class snake(object):
     def __init__(self, color, pos):
         pass
 
@@ -31,11 +31,22 @@ class Snake(object):
     def draw(self, surface):
         pass
 
-def drawGrid(w, rows, surface):
-    pass    
+def drawGrid(width, rows, surface):
+    sizeBtwn = width // rows
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+    
+        pygame.draw.line(surface, (255,255,255), (x, 0), (x,width))
+        pygame.draw.line(surface, (255,255,255), (0, y), (width,y))
+        
 
-def redrawWindow(surface):
-    pass
+def redrawWindow(surface, rows, width):
+    surface.fill((0,0,0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
 
 def randomSnack(rows, item):
     pass
@@ -45,8 +56,17 @@ def messagebox(subject, content):
 
 def main():
     width = 500
-    height = 50
-    win = pygame.display.set_mode((width.height))
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s = snake((255,0,0), (10,10))
+    flag = True
+
+    clock = pygame.time.Clock()
+    
+    while flag:
+        pygame.time.delay(30)
+        clock.tick(10)
+        redrawWindow(win, rows, width)
     pass
 
 main()
